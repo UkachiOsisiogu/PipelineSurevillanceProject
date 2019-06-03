011f2ec05df20a250a905e32ad7fdf12f2a84296
kzish <zishumbak@gmail.com>
2019-06-03 08:58:21 +0100

    Add project files.

---
+++ DroneWebConsole/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DroneWebConsole.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
